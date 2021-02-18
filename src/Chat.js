c425bfe191973009f663686e10c6c1e7203cf22d
cankush625 <cankush625@gmail.com>
2021-02-19 01:49:35 +0530

    Chat screen implemeted
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,69 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@material-ui/icons';
+import MicIcon from "@material-ui/icons/Mic";
 import React from 'react';
-import "./Chat.css";
+import './Chat.css';
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>Chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Ankush Chavan
+                    </span>
+                    Hello
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message chat__receiver">
+                    <span className="chat__name">
+                        Ankush Chavan
+                    </span>
+                    Hello
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon />
+                <form>
+                    <input
+                        placeolder="Type a message"
+                        type="text"
+                    />
+                    <button
+                        type="submit">
+                            Send a message
+                    </button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     );
 }
